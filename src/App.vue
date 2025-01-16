189f8713bb11209384cec2ccd7036525f5f74b23
chenmeng.txt <cm123_meng@163.com>
2025-01-16 19:03:36 +0800

    添加前端代码库

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #e70909;
+  margin-top: 60px;
+  color: rgb(157, 139, 199);
+}
+</style>
